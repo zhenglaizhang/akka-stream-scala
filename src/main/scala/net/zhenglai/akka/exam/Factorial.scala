72f64b34725dd3faf794995eb9500591670383bb
Zhenglai Zhang <zhenglai.zhang@vpom.com>
2016-10-03 17:47:49 +0800

    Add more akka streaming testing code

--- src/main/scala/net/zhenglai/akka/exam/Factorial.scala
+++ src/main/scala/net/zhenglai/akka/exam/Factorial.scala
@@ -4,9 +4,10 @@ import java.nio.file.Paths
 
 import scala.concurrent.Future
 
-import akka.NotUsed
+import akka.{Done, NotUsed}
 import akka.actor.ActorSystem
-import akka.stream.{ActorMaterializer, IOResult}
+import scala.concurrent.duration._
+import akka.stream.{ActorMaterializer, IOResult, ThrottleMode}
 import akka.stream.scaladsl.{FileIO, Flow, Keep, Sink, Source}
 import akka.util.ByteString
 
@@ -17,19 +18,36 @@ object Factorial extends App {
       .map(s => ByteString(s"$s\n"))
       .toMat(FileIO.toPath(Paths.get(fileName)))(Keep.right)
 
-  implicit val actorSystem = ActorSystem("quick-start");
+  implicit val system = ActorSystem("quick-start");
   implicit val materializer = ActorMaterializer()
   val source: Source[Int, NotUsed] = Source(1 to 100)
+
+  // shortcut version
   source.runForeach(println)
+  println("full version ...")
+  Source(1 to 10).runWith(Sink.foreach(println))
+  println("full version done...")
 
   val factorials = source.scan(BigInt(1))((acc, next) => acc * next)
   val result: Future[IOResult] =
     factorials
       .map(num => ByteString(s"$num\n"))
-      .runWith(FileIO.toPath(Paths.get("factorials.txt")))
+      .runWith(FileIO.toPath(Paths.get("target/factorials.txt")))
 
-  factorials.map(_.toString).runWith(lineSink("factorials2.txt"))
+  factorials.map(_.toString).runWith(lineSink("target/factorials2.txt"))
 
 
   println("DONE...")
+
+  (1 to 10).foreach(println)
+
+  val done: Future[Done] =
+    factorials
+      .zipWith(Source(0 to 20))((num, idx) => s"$idx! = $num")
+      .throttle(1, 1 second, 1, ThrottleMode.shaping)
+      .runForeach(println)
+
+  Thread.sleep(10000)
+  system.terminate()
+  println("DONE")
 }
